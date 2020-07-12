94368e91fe672dbec6ff023bd1ce7ee2e0272b98
Calista Huang <calistahuang@calistas-air.attlocal.net>
2020-07-11 17:58:03 -0700

    Max characters per line is 80.

--- 2012_11_Find_the_Cow/USACO_find_the_cow.java
+++ 2012_11_Find_the_Cow/USACO_find_the_cow.java
@@ -8,10 +8,12 @@ public class USACO_find_the_cow {
         int length = grass_pattern.length();
         int cow_counter = 0;
         for (int i = 1; i < length; i++) {
-            if (grass_pattern.charAt(i - 1) == '(' && grass_pattern.charAt(i) == '(') {
+            if (grass_pattern.charAt(i - 1) == '(' &&
+                grass_pattern.charAt(i) == '(') {
                 // Found hind legs.
                 for (int j = i + 2; j < length; j++) {
-                    if (grass_pattern.charAt(j - 1) == ')' && grass_pattern.charAt(j) == ')') {
+                    if (grass_pattern.charAt(j - 1) == ')' &&
+                        grass_pattern.charAt(j) == ')') {
                         // Found matching front legs; increment cow counter.
                         cow_counter++;
                     }
@@ -26,14 +28,17 @@ public class USACO_find_the_cow {
         int cow_counter = 0;
         int hind_legs_counter = 0;
         for (int i = 1; i < grass_pattern.length(); i++) {
-            if (grass_pattern.charAt(i - 1) == '(' && grass_pattern.charAt(i) == '(') {
+            if (grass_pattern.charAt(i - 1) == '(' &&
+                grass_pattern.charAt(i) == '(') {
                 // Found hind legs; track number of hind legs found.
                 hind_legs_counter++;
             }
-            else if (grass_pattern.charAt(i - 1) == ')' && grass_pattern.charAt(i) == ')') {
-                // Found front legs. This pair of front legs can be combined with any other pair of hind legs to
-                // form a complete cow.
-                // Add number of hind legs cow counter for every pair of front legs found so far.
+            else if (grass_pattern.charAt(i - 1) == ')' &&
+                     grass_pattern.charAt(i) == ')') {
+                // Found front legs. This pair of front legs can be combined
+                // with any other pair of hind legs to form a complete cow.
+                // Add number of hind legs cow counter for every pair of front
+                // legs found so far.
                 cow_counter += hind_legs_counter;
             }
         }
